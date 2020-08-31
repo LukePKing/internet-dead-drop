cc3bbdcaf21f1ad179b66aa9c3218416a106ced7
Luke King <luke-king@hotmail.com>
2020-08-31 20:32:38 +0100

    Made storing of messages with a uuidv4. Currently unencrypted

--- index.js
+++ index.js
@@ -6,7 +6,11 @@
 const express = require("express");
 const fs = require("fs");
 const mysql = require("mysql");
+const crypto = require("crypto");
+const {v4: uuidv4 } = require("uuid");
+const formidableMiddleware = require("express-formidable");
 
+// Temp solution for now
 const testMessageMap = new Map();
 
 /**
@@ -16,6 +20,7 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || "8000";
 app.set("view engine", "pug");
+app.use(formidableMiddleware());
 
 /**
  * App Configuration
@@ -36,7 +41,13 @@ app.get("/message", function(req, res) {
 })
 
 app.post("/", function (req, res) {
-    res.send("POST request to homepage");
+    // Randomly generate an id
+    let msgId = uuidv4();
+
+    // Store in Map (for now)
+    let message = req.fields.message;
+    testMessageMap.set(msgId, message);
+    res.send("POST request to homepage. Your messsage id is: " + msgId);
 })
 
 /**
